603cc37010504fe59224a8c798c622ac98e09af4
Joeshmoe03 <liemjt@comcast.net>
2024-07-11 11:08:28 -0700

    Renaming of some blocks

--- E2CNN_models.py
+++ E2CNN_models.py
@@ -7,7 +7,7 @@ import e2cnn.nn as enn
 
 # More information on the E2CNN library can be found here: https://github.com/QUVA-Lab/e2cnn
 
-class RegBlockRN18(enn.EquivariantModule):
+class BlockRN18(enn.EquivariantModule):
     '''
     A single block of the ResNet18 model equivariant to rotations of 90 degrees using the E2CNN library.
     The block consists of two convolutional layers with batch normalization and ReLU activation functions.
@@ -32,7 +32,7 @@ class RegBlockRN18(enn.EquivariantModule):
                  kernel_size = 3,
                  padding = 1,
                 ):
-        super(RegBlockRN18, self).__init__()
+        super(BlockRN18, self).__init__()
 
         # if outtype is not specified, it is an intermediate block
         if out_type is None:
@@ -149,17 +149,17 @@ class RegRepRN18(tnn.Module):
         )
 
         self._in_type = self.inblock1.out_type
-        self.layer2 = self._make_layer(RegBlockRN18, self.stages[0], self.n[0])
+        self.layer2 = self._make_layer(BlockRN18, self.stages[0], self.n[0], kernel_size = kernel_size, padding = padding)
 
         self._in_type = self.layer2.out_type
-        self.layer3 = self._make_layer(RegBlockRN18, self.stages[1], self.n[1])
+        self.layer3 = self._make_layer(BlockRN18, self.stages[1], self.n[1], kernel_size = kernel_size, padding = padding)
 
         self._in_type = self.layer3.out_type
-        self.layer4 = self._make_layer(RegBlockRN18, self.stages[2], self.n[2])
+        self.layer4 = self._make_layer(BlockRN18, self.stages[2], self.n[2], kernel_size = kernel_size, padding = padding)
 
         # The last layer has the trivial representation as the output
         self._in_type = self.layer4.out_type
-        self.layer5 = self._make_layer(RegBlockRN18, self.stages[3], self.n[3], totrivial = True)
+        self.layer5 = self._make_layer(BlockRN18, self.stages[3], self.n[3], kernel_size = kernel_size, padding = padding, totrivial = True)
 
         self.linear = tnn.Linear(self.layer5.out_type.size, n1)
 
