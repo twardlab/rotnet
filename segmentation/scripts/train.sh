edb8fc3bd513ac46ca6b2e1627f583fcbbe07707
Joeshmoe03 <liemjt@comcast.net>
2024-08-02 15:44:15 -0700

    script

--- segmentation/scripts/train.sh
+++ segmentation/scripts/train.sh
@@ -4,7 +4,7 @@
 #SBATCH --mem-per-cpu 30G
 #SBATCH --time 24:00:00
 #SBATCH --cpus-per-task 3
-#SBATCH --job-name equiv_benchmark
+#SBATCH --job-name equiv_segm
 #SBATCH --output segm.log
 
 # load modules or conda environments here
@@ -12,4 +12,4 @@ module load cerebrum-hpc/1.0
 module load anaconda/3.7
 source /nafs/dtward/intelpython3/bin/activate
 
-python ../segmentation_train.py --data_flag ... --label_flag ...
+python ../segmentation_train.py 
